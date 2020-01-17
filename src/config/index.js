0922e7bf4f79410cbf94dae529ae047d5db19551
alan <alanarostegui@hotmail.com>
2020-01-17 00:49:51 -0600

    refactor y conectando a una db mongo

--- src/config/index.js
+++ src/config/index.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const config = {
-  port: process.env.PORT || 3000
+  port: process.env.PORT || 3000,
+  dbUrl: process.env.DB_URL
 };
 
 module.exports = { config };
--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
-const { config } = require('./config/index.js')
-const app = require('./index');
+const { config } = require('./config/index.js');
+const express = require('express');
+const app = express();
+const db = require('./db');
+
+db(config.dbUrl);
 
 async function main() {
     await app.listen(config.port)
